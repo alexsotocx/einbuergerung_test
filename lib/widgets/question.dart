2dc077f11058231855453ead6b1907ccbe467142
Alexander Soto <alexsotocx@gmail.com>
2023-05-23 11:32:13 +0200

    Add question test

--- lib/widgets/question.dart
+++ lib/widgets/question.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 
 class QuestionWidget extends StatefulWidget {
   final Question question;
-  final void Function() onNext;
-  final void Function(bool correct) onSelectAnswer;
-  final void Function() onPrevious;
+  final void Function()? onNext;
+  final void Function(bool correct, String selected) onSelectAnswer;
+  final void Function()? onPrevious;
+  final String? initialAnswer;
 
   const QuestionWidget({
     super.key,
     required this.question,
     required this.onSelectAnswer,
-    required this.onNext,
-    required this.onPrevious,
+    this.onNext,
+    this.onPrevious,
+    this.initialAnswer,
   });
 
   @override
@@ -24,6 +26,21 @@ class QuestionWidget extends StatefulWidget {
 class _QuestionWidgetState extends State<QuestionWidget> {
   String? _selectedAnswer;
   bool _answered = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedAnswer = widget.initialAnswer;
+    _answered = widget.initialAnswer != null;
+  }
+
+  TextStyle? getTextStyle(bool correct) {
+    if (!_answered) return null;
+    return TextStyle(
+      color: correct ? Colors.green : Colors.red,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -41,6 +58,8 @@ class _QuestionWidgetState extends State<QuestionWidget> {
                   key: Key(questionOption),
                   title: Text(
                     questionOption,
+                    style:
+                        getTextStyle(questionOption == widget.question.answer),
                   ),
                   value: questionOption,
                   groupValue: _selectedAnswer,
@@ -57,17 +76,31 @@ class _QuestionWidgetState extends State<QuestionWidget> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  if (_answered && widget.onPrevious != null)
+                    ElevatedButton(
+                        key: const Key('on_previous'),
+                        onPressed: widget.onPrevious,
+                        child: const Text('Previous')),
                   ElevatedButton(
+                      key: const Key('on_check'),
                       onPressed: _answered
                           ? null
                           : () {
                               setState(() {
                                 _answered = true;
                               });
+
                               widget.onSelectAnswer(
-                                  widget.question.answer == _selectedAnswer);
+                                widget.question.answer == _selectedAnswer,
+                                _selectedAnswer!,
+                              );
                             },
-                      child: const Text('Check'))
+                      child: const Text('Check')),
+                  if (_answered && widget.onNext != null)
+                    ElevatedButton(
+                        key: const Key('on_next'),
+                        onPressed: widget.onNext,
+                        child: const Text('Next')),
                 ],
               )
             ],
--- test/question_test.dart
+++ test/question_test.dart
@@ -7,7 +7,7 @@ import 'package:mockito/mockito.dart';
 
 class _MockFunctions extends Mock {
   void onNext();
-  void onSelectAnswer(bool correct);
+  void onSelectAnswer(bool correct, String answer);
   void onPrevious();
 }
 
@@ -54,7 +54,74 @@ void main() {
     expect(find.text(question.text), findsOneWidget);
   });
 
+  group('action buttons', () {
+    testWidgets('button is rendered', (tester) async {
+      var mocksCallbacks = _MockFunctions();
+
+      await tester.pumpWidget(createTestWidget([
+        QuestionWidget(
+          question: question,
+          onNext: mocksCallbacks.onNext,
+          onPrevious: mocksCallbacks.onPrevious,
+          onSelectAnswer: mocksCallbacks.onSelectAnswer,
+          initialAnswer: question.answer,
+        ),
+      ]));
+
+      expect(find.byKey(const Key('on_next')), findsOneWidget);
+      expect(find.byKey(const Key('on_previous')), findsOneWidget);
+    });
+
+    group('when on next/previous callback is not passed', () {
+      testWidgets('button is not redered', (tester) async {
+        var mocksCallbacks = _MockFunctions();
+
+        await tester.pumpWidget(createTestWidget([
+          QuestionWidget(
+            question: question,
+            onNext: null,
+            onPrevious: null,
+            onSelectAnswer: mocksCallbacks.onSelectAnswer,
+          ),
+        ]));
+
+        expect(find.byKey(const Key('on_next')), findsNothing);
+        expect(find.byKey(const Key('on_previous')), findsNothing);
+      });
+    });
+  });
+
   group('CheckAnswer', () {
+    testWidgets('show what are the wrong answers and correct one',
+        (tester) async {
+      final mocksCallbacks = _MockFunctions();
+
+      await tester.pumpWidget(createTestWidget([
+        QuestionWidget(
+          question: question,
+          onNext: mocksCallbacks.onNext,
+          onPrevious: mocksCallbacks.onPrevious,
+          onSelectAnswer: mocksCallbacks.onSelectAnswer,
+        ),
+      ]));
+
+      var correctAnsOption = find.byKey(Key(question.answer));
+      await tester.tap(correctAnsOption);
+
+      final buttonWidget = findButtonByText('Check');
+
+      await tester.tap(buttonWidget);
+
+      await tester.pumpAndSettle();
+
+      for (final q in question.options) {
+        final optionWidget =
+            tester.widget<RadioListTile>(find.byKey(Key(q))).title as Text;
+        expect(optionWidget.style?.color,
+            q == question.answer ? Colors.green : Colors.red);
+      }
+    });
+
     group('when answer is correct', () {
       testWidgets('marks the answer as correct', (tester) async {
         final mocksCallbacks = _MockFunctions();
@@ -74,7 +141,7 @@ void main() {
         final buttonWidget = findButtonByText('Check');
 
         await tester.tap(buttonWidget);
-        verify(mocksCallbacks.onSelectAnswer(true));
+        verify(mocksCallbacks.onSelectAnswer(true, question.answer));
 
         await tester.pumpAndSettle();
 
@@ -84,7 +151,7 @@ void main() {
       });
     });
 
-    group('when answer is false', () {
+    group('when answer is not correct', () {
       testWidgets('marks the answer as not correct', (tester) async {
         final mocksCallbacks = _MockFunctions();
 
@@ -97,14 +164,15 @@ void main() {
           ),
         ]));
 
-        final falseOption = find.byKey(Key(question.options
-            .firstWhere((element) => question.answer != element)));
+        final wrongAnswerText = question.options
+            .firstWhere((element) => question.answer != element);
+        final falseOption = find.byKey(Key(wrongAnswerText));
         await tester.tap(falseOption);
 
         final buttonWidget = findButtonByText('Check');
 
         await tester.tap(buttonWidget);
-        verify(mocksCallbacks.onSelectAnswer(false));
+        verify(mocksCallbacks.onSelectAnswer(false, wrongAnswerText));
 
         await tester.pumpAndSettle();
 
